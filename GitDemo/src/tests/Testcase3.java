b165924fea7e5d64460cfb88a515a8cb371ad713
SANGEETHA <SANGEETHA@192.168.1.5>
2021-05-20 19:26:55 +0530

    third test case added by sangeetha

---
+++ GitDemo/src/tests/Testcase3.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Testcase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.print("This is test caes3");
+	}
+
+}
